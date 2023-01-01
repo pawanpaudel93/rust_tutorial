6493a4da25206d4358e9cd16c5b56b7cf4b238ab
Pawan Paudel <pawanpaudel93@gmail.com>
2023-01-01 22:51:36 +0545

    Completed closures

--- src/main.rs
+++ src/main.rs
@@ -374,6 +374,33 @@ fn iterators() {
     println!("1st: {:?}", iter1.next());
 }
 
+fn closuers() {
+    let can_vote = |age: i32| age >= 18;
+    println!("Can vote: {}", can_vote(8));
+
+    let mut samp1 = 5;
+    let print_var = || println!("samp1 = {}", samp1);
+    print_var();
+    samp1 = 10;
+    let mut change_var = || samp1 += 1;
+    change_var();
+    println!("samp1 = {}", samp1);
+    samp1 = 10;
+    println!("samp1 = {}", samp1);
+
+    // passing closures into a function
+    fn use_func<T>(a: i32, b: i32, func: T) -> i32
+    where
+        T: Fn(i32, i32) -> i32,
+    {
+        func(a, b)
+    }
+    let sum = |a, b| a + b;
+    let product = |a, b| a * b;
+    println!("5 + 4 = {}", use_func(5, 4, sum));
+    println!("5 * 4 = {}", use_func(5, 4, product));
+}
+
 fn main() {
     // variables()
     // constants()
@@ -392,4 +419,5 @@ fn main() {
     // order_food();
     // read_write_files()
     // iterators()
+    // closuers()
 }
